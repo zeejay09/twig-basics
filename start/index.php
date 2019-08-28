8887ba59c58537b28e56d1ee41e4d84b817f5506
Jeff Zeejay <zeejaybelamide@gmail.com>
2019-08-28 14:00:00 +0800

    Chapter 3 Using Objects and Arrays finished.

--- start/index.php
+++ start/index.php
@@ -32,14 +32,18 @@ switch ($uri) {
     // The Homepage! (/)
     case '/':
         echo $twig->render('homepage.twig', array(
-            'pageTitle' => 'Suit Up!',
+            'pageData' => array(
+                'title'    => 'Suit Up!',
+                'summary'  => 'You\'re hip, you\'re cool, you\'re a penguin! Now, start dressing like one! Find the latest suits, bow-ties, swim shorts and other outfits here!',
+                'hasSale'  => 1,
+            ),
             'products' => array(
-                'Serious Businessman',
-                'Penguin Dress',
-                'Sportstar Penguin',
-                'Angel Costume',
-                'Penguin Accessories',
-                'Super Cool Penguin',
+                new Product('Serious Businessman', 'formal.png'),
+                new Product('Penguin Dress', 'dress.png'),
+                new Product('Sportstar Penguin', 'sports.png'),
+                new Product('Angel Costume', 'angel-costume.png'),
+                new Product('Penguin Accessories', 'swatter.png'),
+                new Product('Super Cool Penguin', 'super-cool.png'),
             ),
             'saleEndsAt' => new\DateTime('+1 month'),
         ));
